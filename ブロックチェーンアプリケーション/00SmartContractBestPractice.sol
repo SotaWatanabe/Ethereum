501ace0ef4fb15fbca87cf2aae9561e018b85421
Sota Watanabe <w.souta.s.life@gmail.com>
2018-05-21 10:01:24 +0900

    add

---
+++ ブロックチェーンアプリケーション/00SmartContractBestPractice.sol
@@ -0,0 +1,21 @@
+Etheruem Smart Contract Best Practice
+https://consensys.github.io/smart-contract-best-practices/recommendations/
+
+//External Calls
+Use caution when making external calls
+
+//Mark untrusted contracts
+// bad
+Bank.withdraw(100); // Unclear whether trusted or untrusted
+
+function makeWithdrawal(uint amount) { // Isn't clear that this function is potentially unsafe
+    Bank.withdraw(amount);
+}
+
+// good
+UntrustedBank.withdraw(100); // untrusted external call
+TrustedBank.withdraw(100); // external but trusted bank contract maintained by XYZ Corp
+
+function makeUntrustedWithdrawal(uint amount) {
+    UntrustedBank.withdraw(amount);
+}
